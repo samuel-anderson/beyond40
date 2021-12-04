07413dca653f78589e779fc84da64c454c3d483b
samuel-anderson <s07241990@gmail.com>
2021-12-04 14:03:40 -0600

    ratings section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function(){
 
     const data = {
+        totalReviews: 3219,
         fiveStar: 2928,
         fourStar: 229,
         threeStar: 50,
@@ -12,6 +13,7 @@ $(function(){
 
 
     const populateRatingCounts = () => {
+        $("#totalReviews").text(data.totalReviews);
         $("#5-star-count").text(data.fiveStar);
         $("#4-star-count").text(data.fourStar)
         $("#3-star-count").text(data.threeStar)
